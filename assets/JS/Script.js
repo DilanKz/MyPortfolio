41816cc12578d06572325de7a63b87643ed9ca71
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-06-26 21:55:02 +0530

    update drop menu

--- assets/JS/Script.js
+++ assets/JS/Script.js
@@ -1,6 +1,20 @@
-var loader=document.getElementById("preLoader");
+let loader=document.getElementById("preLoader");
+let toggleButton=document.getElementById("toggleButton");
+let navMenu=document.getElementById("navMenu");
 
 window.addEventListener("load",function (){
     loader.style.display= "none";
 })
 
+let toggle=false;
+toggleButton.addEventListener('click',function (){
+   if (toggle===false){
+       toggleButton.className = 'fa-solid fa-x';
+       navMenu.style.transform = 'translate(3.5vh, 3vh)';
+       toggle = true;
+   }else if (toggle===true){
+       toggleButton.className = 'fa-solid fa-bars';
+       navMenu.style.transform = 'translate(3.5vh, -50vh)';
+       toggle = false;
+   }
+});
